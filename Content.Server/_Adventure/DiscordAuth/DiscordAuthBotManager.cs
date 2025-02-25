fc4f44f4a005b54a9e18f0ef60411c1f44ce6a88
c4llv07e <igor@c4llv07e.xyz>
2025-02-26 00:27:51 +0300

    More progress

--- Content.Server/_Adventure/DiscordAuth/DiscordAuthBotManager.cs
+++ Content.Server/_Adventure/DiscordAuth/DiscordAuthBotManager.cs
@@ -82,18 +82,40 @@ public sealed class DiscordAuthBotManager
             rqArgs["redirect_uri"] = listeningUrl;
 
             Console.WriteLine("Sending request");
-            using var msg = new HttpRequestMessage(HttpMethod.Post, "oauth2/token")
+            using var getTokenMsg = new HttpRequestMessage(HttpMethod.Post, "oauth2/token")
             {
                 Content = new FormUrlEncodedContent(rqArgs),
             };
-            using HttpResponseMessage response = await discordClient.SendAsync(msg);
+            using HttpResponseMessage response = await discordClient.SendAsync(getTokenMsg);
             Console.WriteLine("Request sent");
             var str = await response.Content.ReadAsStringAsync();
             Console.WriteLine($"str: {str}");
             var res = JsonSerializer.Deserialize<TokenResponse>(str);
-            if (res is null) return;
+            if (res is null)
+            {
+                resp.StatusCode = (int) HttpStatusCode.Forbidden;
+                resp.StatusDescription = "Forbidden";
+                WriteStringStream(resp, $"Error {str}");
+                return;
+            }
 
             Console.WriteLine($"{res.access_token} {res.token_type}");
+
+            using var getUserMsg = new HttpRequestMessage(HttpMethod.Get, "users/@me");
+            getUserMsg.Headers.Authorization = new AuthenticationHeaderValue(res.token_type, res.access_token);
+            using HttpResponseMessage userResp = await discordClient.SendAsync(getUserMsg);
+            var userRespStr = await userResp.Content.ReadAsStringAsync();
+            Console.WriteLine($"user resp: {userRespStr}");
+            var userRespRes = JsonSerializer.Deserialize<UserResponse>(userRespStr);
+            if (userRespRes is null)
+            {
+                resp.StatusCode = (int) HttpStatusCode.Forbidden;
+                resp.StatusDescription = "Forbidden";
+                WriteStringStream(resp, $"Error {userRespStr}");
+                return;
+            }
+
+            Console.WriteLine($"user id: {userRespRes.id}");
 
             resp.StatusCode = (int) HttpStatusCode.OK;
             resp.StatusDescription = "OK";
@@ -114,14 +136,20 @@ public sealed class DiscordAuthBotManager
     }
 
     public record class OauthTokenRequest(
-        string grant_type,
-        string code,
-        string redirect_uri);
+        string? grant_type = null,
+        string? code = null,
+        string? redirect_uri = null,
+        string? state = null);
 
+    // {"token_type": "Bearer", "access_token": "ibnjoi44JCapPRWRDU4EPtE3slJFWC", "expires_in": 604800, "refresh_token": "Ljk03G6mG6du1Lo6yazxrQ6Se7oLY1", "scope": "identify"}
     public record class TokenResponse(
         string token_type = "Bearer",
         string? access_token = null,
         int expires_in = 0,
         string? refresh_token = null,
-        string scope = "identify");
+        string scope = "identify",
+        string? state = null);
+
+    // {"id":"642524678136659968","username":"c4llv07e","avatar":"417944fb9465a53484dd8f6b4282c580","discriminator":"0","public_flags":0,"flags":0,"banner":null,"accent_color":null,"global_name":"c4llv07e","avatar_decoration_data":null,"banner_color":null,"clan":null,"primary_guild":null,"mfa_enabled":true,"locale":"en-US","premium_type":0}
+    public record class UserResponse(string? id = null);
 }
