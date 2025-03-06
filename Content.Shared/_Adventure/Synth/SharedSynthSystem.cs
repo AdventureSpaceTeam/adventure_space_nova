5183f7e0d86825048a7447aeb59eca70dfe87b98
Bork Roman <bork.roman@bk.ru>
2025-03-06 18:27:06 +0700

    всё....
    исправлен баг из-за которого кпб не могли заряжаться

--- Content.Shared/_Adventure/Synth/SharedSynthSystem.cs
+++ Content.Shared/_Adventure/Synth/SharedSynthSystem.cs
@@ -73,6 +73,6 @@ public abstract partial class SharedSynthSystem : EntitySystem
             return;
 
         var sprintDif = movement.BaseWalkSpeed / movement.BaseSprintSpeed;
-        args.ModifySpeed(0.5f, sprintDif);
+        args.ModifySpeed(0.5f, 0.5f);
     }
 }
